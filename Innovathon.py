867506cdc0470f03517c5c7ad73d083d43be95d5
giulioma22 <giuliomartinelli22@gmail.com>
2019-11-16 22:41:44 +0100

    Add line detector

--- Innovathon.py
+++ Innovathon.py
@@ -2,17 +2,24 @@ import numpy as np
 import cv2 as cv
 from time import sleep
 
-cap = cv.VideoCapture("white_line_1.mp4")
+# cap = cv.VideoCapture("white_line_1.mp4")
+# cap = cv.VideoCapture("white_line_2.mp4")
+cap = cv.VideoCapture("white_line_3.mp4")
+# cap = cv.VideoCapture("white_line_4.mp4")
 
-for i in range(100000):
+for i in range(1000):
     ret, frame = cap.read()
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    lower_blue = np.array([0,0,200])
-    upper_blue = np.array([255,255,255])
+    lower_blue = np.array([0,0,190])
+    upper_blue = np.array([255,255,225])
     mask = cv.inRange(hsv, lower_blue, upper_blue)
+    edges = cv.Canny(mask,50,150,apertureSize = 3)
 
-    cv.imshow("Frame", mask)
-    sleep(0.01)
+    # cv.imshow("Normal", frame)
+    cv.imshow("White line", mask)
+    # cv.imshow("Frame", edges)
+    sleep(0.05)
+
     if cv.waitKey(1) & 0xFF == ord("q"):
         break
 
